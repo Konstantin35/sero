e1dac74f6f57db87ca061416842cffd3daba9d23
xiangqh <xiang@xiangqinghuadeMacBook-Pro.local>
2018-09-25 16:16:37 +0800

    update

--- install.sh
+++ install.sh
@@ -8,8 +8,8 @@ git clone git@github.com:sero-cash/go-czero-import.git
 git clone git@github.com:sero-cash/go-sero.git
 cd "$git_path/go-czero-import/czero/lib"
 ln -s "$root_path/lib/libczero.so" libczero.so
+cd $root_path
+ln -s "src/github.com/sero-cash/go-czero-import/czero/data" data
+ln -s "src/github.com/sero-cash/go-sero/build/bin/gero" gero
 cd "$git_path/go-sero"
 make all
-cd "$root_path"
-ln -s "$git_path/go-sero/build/bin/gero" gero
-ln -s "$git_path/go-czero-import/czero/data" data
