0c00a9cd137a11a66121ae44f22f433b48f3c3e8
xiangqh <xiang@xiangqinghuadeMacBook-Pro.local>
2018-09-25 16:35:52 +0800

    update

--- install.sh
+++ install.sh
@@ -4,12 +4,34 @@ root_path="$PWD"
 mkdir -p "src/github.com/sero-cash"
 git_path="$root_path/src/github.com/sero-cash"
 cd "$git_path"
-git clone git@github.com:sero-cash/go-czero-import.git
-git clone git@github.com:sero-cash/go-sero.git
+if [ ! -d "/go-sero/" ];then
+	git clone git@github.com:sero-cash/go-sero.git
+else
+	cd "go-sero"
+	git fetch&&git rebase
+fi
+cd "$git_path"
+
+if [ ! -d "/go-czero-import/" ];then
+	git clone git@github.com:sero-cash/go-czero-import.git
+else
+	cd "go-czero-import"
+	git fetch&&git rebase
+fi
+
 cd "$git_path/go-czero-import/czero/lib"
-ln -s "$root_path/lib/libczero.so" libczero.so
+if [ ! -f "libczero.so" ];then
+	ln -s "$root_path/lib/libczero.so" libczero.so
+fi
+
 cd $root_path
-ln -s "src/github.com/sero-cash/go-czero-import/czero/data" data
-ln -s "src/github.com/sero-cash/go-sero/build/bin/gero" gero
+if [ !  -d "/data/" ];then
+	ln -s "src/github.com/sero-cash/go-czero-import/czero/data" data
+fi
+
+if [ !  -f "gero" ];then
+	ln -s "src/github.com/sero-cash/go-sero/build/bin/gero" gero
+fi
+
 cd "$git_path/go-sero"
 make all
