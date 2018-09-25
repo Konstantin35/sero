95077e140384367a1f084298ba5f2855f2c8ffed
xiangqh <xiang@xiangqinghuadeMacBook-Pro.local>
2018-09-25 13:14:58 +0800

    update

---
+++ dependencies.sh
@@ -0,0 +1,14 @@
+sudo yum install gcc -y
+sudo yum install cmake -y
+sudo yum install gcc-c++ -y
+sudo yum install openssl-devel -y
+sudo yum install boost -y
+sudo yum install boost-devel -y
+sudo yum install gmp -y
+sudo yum install openmp -y
+sudo yum install libomp -y
+sudo yum install libgomp -y
+sudo yum install gmp-devel -y
+sudo yum install libtool -y
+sudo yum install gmp-devel
+sudo yum install libtool -y
