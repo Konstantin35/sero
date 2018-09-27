055ade39b095792f8573272a0dbc5fd500147d2e
huma <huma@sero.vip>
2018-09-27 14:40:30 +0800

    update

--- start.sh
+++ start.sh
@@ -12,6 +12,5 @@ fi
 for i in $(seq 0 $total)  
 do   
 echo "start $i"  
-	echo $LD_LIBRARY_PATH
 	nohup  bin/gero --datadir "chain_$i" --port $(expr $i + 30310)  --rpc --rpcport $(expr $i + 8545) --rpcaddr "0.0.0.0" --rpccorsdomain "*" --dev --devpassword 123456 --gcmode archive --rpcapi 'personal,db,sero,net,web3,txpool,miner' > log/$i.log &
 done
