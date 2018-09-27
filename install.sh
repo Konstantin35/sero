33eb0e76c81eb551877ff22380857602067170f7
huma <huma@sero.vip>
2018-09-27 13:46:06 +0800

    update

--- install.sh
+++ install.sh
@@ -4,7 +4,7 @@ root_path="$PWD"
 mkdir -p "src/github.com/sero-cash"
 git_path="$root_path/src/github.com/sero-cash"
 cd "$git_path"
-if [ ! -d "/go-sero/" ];then
+if [ ! -d "go-sero" ];then
 	git clone git@github.com:sero-cash/go-sero.git
 else
 	cd "go-sero"
@@ -12,7 +12,7 @@ else
 fi
 cd "$git_path"
 
-if [ ! -d "/go-czero-import/" ];then
+if [ ! -d "go-czero-import" ];then
 	git clone git@github.com:sero-cash/go-czero-import.git
 else
 	cd "go-czero-import"
@@ -25,11 +25,11 @@ if [ ! -L "libczero.so" ];then
 fi
 
 cd $root_path
-if [ ! -L "/data/" ];then
+if [ ! -L "data" ];then
 	ln -s "src/github.com/sero-cash/go-czero-import/czero/data" data
 fi
 
-if [ ! -L "/bin/" ];then
+if [ ! -L "bin" ];then
 	ln -s "src/github.com/sero-cash/go-sero/build/bin" bin
 fi
 
