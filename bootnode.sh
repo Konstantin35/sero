c850e51e446b3f036bf4b1199f3572492a4eb2d5
huma <huma@sero.vip>
2018-09-29 14:43:03 +0800

    update

--- bootnode.sh
+++ bootnode.sh
@@ -1,2 +1,2 @@
 #!/bin/bash
-nohup bin/bootnode -nodekey=bootnode.key -verbosity=9 -addr :60609 -nat=any > bootnode.log 2>1&
+nohup bin/bootnode -nodekey=bootnode.key -verbosity=9 -addr :60609 -nat=any > bootnode.log &
