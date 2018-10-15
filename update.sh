34ee1d3e172e4a723f869b6902571e0381c903ee
huma <huma@sero.vip>
2018-10-15 23:42:45 +0800

    update

--- update.sh
+++ update.sh
@@ -1,9 +1,7 @@
+cd "$git_path/go-czero-import"
+git fetch&&git rebase
 
 git_path="$PWD/src/github.com/sero-cash"
 cd "$git_path/go-sero"
 git fetch&&git rebase
 make all
-
-cd "$git_path/go-czero-import"
-git fetch&&git rebase
-
