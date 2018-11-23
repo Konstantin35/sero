f36889cbe2600cd0fd9df7d0452d448b8d004c91
huma <huma@sero.vip>
2018-11-24 00:16:15 +0800

    update

--- install.sh
+++ install.sh
@@ -8,7 +8,7 @@ mkdir -p "src/github.com/sero-cash"
 git_path="$root_path/src/github.com/sero-cash"
 cd "$git_path"
 if [ ! -d "go-czero-import" ];then
-	git clone git@github.com:sero-cash/go-czero-import.git
+	git clone https://github.com/sero-cash/go-czero-import.git
 else
 	cd "go-czero-import"
 	git fetch&&git rebase
@@ -16,7 +16,7 @@ fi
 
 cd "$git_path"
 if [ ! -d "go-sero" ];then
-	git clone git@github.com:sero-cash/go-sero.git
+	git clone https://github.com/sero-cash/go-sero.git
 else
 	cd "go-sero"
 	git fetch&&git rebase
