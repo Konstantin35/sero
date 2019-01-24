a9ac359da2784d69360e14d4448d36a3cc393c99
huma <huma@sero.vip>
2019-01-24 15:29:14 +0800

    update

--- deploy.sh
+++ deploy.sh
@@ -5,9 +5,12 @@ if [ -n "$1" ]; then
     branch=$1
 fi
 
+flag=false
 root_path="$PWD"
 git_path="$root_path/src/github.com/sero-cash/go-sero"
+
 cd "$git_path"
+git checkout -t "origin/$branch"
 git checkout $branch
 
 cd $root_path
