5d38e67528e77cbfdebb1a8b84651da08b392b6e
huma <huma@sero.vip>
2019-01-24 13:30:12 +0800

    update

--- deploy.sh
+++ deploy.sh
@@ -1,5 +1,15 @@
 #!/bin/bash
 
+branch=master
+if [ -n "$1" ]; then
+    branch=$1
+fi
+
+root_path="$PWD"
+git_path="$root_path/src/github.com/sero-cash/go-sero"
+cd "$git_path"
+git checkout $branch
+
 sh update.sh
 sh clean.sh
 sh restart.sh
