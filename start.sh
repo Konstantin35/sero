cddfdeb1bc28ca211eb2761a1d4c10f6a1725893
huma <huma@sero.vip>
2019-02-21 14:37:19 +0800

    update

--- start.sh
+++ start.sh
@@ -1,4 +1,4 @@
 #!/bin/bash
 
 export LD_LIBRARY_PATH=lib
-nohup  bin/gero --cache 4096 --trie-cache-gens 1200 --maxpeers 256 --datadir chaindata  > log/gero.log 2>&1 &
+nohup  bin/gero --cache 4096 --trie-cache-gens 1200 --maxpeers 256 --config geroConfig.toml --datadir chaindata > log/gero.log 2>&1 &
