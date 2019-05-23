4ca232b25f3441b3365c1e03d18bdc71695f590f
huma <huma@sero.vip>
2019-05-23 22:14:03 +0800

    update

--- start.sh
+++ start.sh
@@ -1,4 +1,4 @@
 #!/bin/bash
 
 export LD_LIBRARY_PATH=lib
-nohup  bin/gero --cache 4096 --trie-cache-gens 1200 --maxpeers 128 --config geroConfig.toml --datadir chaindata --mineMode --rpc --rpcaddr '127.0.0.1' --rpccorsdomain '*' > log/gero.log 2>&1 &
+bin/gero --config geroConfig.toml --port 53718 --maxpeers 256 --vthreads 4 --datadir chaindata --mineMode > log/gero.log 2>&1 &
